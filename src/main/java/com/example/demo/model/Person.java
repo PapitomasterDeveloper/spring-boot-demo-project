2cd78936d4e63a67a82575fc53b6a5cd5c9874d2
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-11-18 00:46:21 -0600

    Added the NotBlank to indicate that there is not allowed to permit an empty value

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -2,11 +2,14 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 public class Person {
 
     private final UUID id;
+
+    @NotBlank  
     private final String name;
 
     // Mapping the property names of the Person constructor with JSON keys
