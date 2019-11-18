867b7f604ee6fb4ec69a0f96d609ae3a05461c65
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-11-17 22:56:12 -0600

    The requested data will be mapped into JSON keys with JsonProperty annotation

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public class Person {
@@ -7,7 +9,9 @@ public class Person {
     private final UUID id;
     private final String name;
 
-    public Person(UUID id, String name){
+    // Mapping the property names of the Person constructor with JSON keys
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name){
         this.id = id;
         this.name = name;
     }
