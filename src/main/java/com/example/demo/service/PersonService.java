22d124ee0849bd2305381d06801967889dec5f46
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-11-17 23:30:57 -0600

    Method to retrieve all the people from the DB, the GET method is already tested with Postman

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class PersonService {
 
@@ -19,7 +22,10 @@ public class PersonService {
     }
 
     public int addPerson(Person person){
-        // return personDao.insertPerson(person);
-        return 0;
+        return personDao.insertPerson(UUID.randomUUID(), person);
+    }
+
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
     }
 }
